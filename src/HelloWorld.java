2e8595e91a9c4a3a5252b2de8f3890f0935b6cfe
xieyuxuan <xieyuxuan@localhost>
2020-11-07 17:45:07 -0500

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please put your name:");
+		String name=sc.nextLine();
+		System.out.println("Hello World, "+name);
+		System.out.println("Please print a number<100: ");
+		int n=sc.nextInt();
+		int accumulator=0;
+		for(int i=1;i<=n;i+=2) {
+			accumulator+=i;
+		}
+		System.out.println(accumulator);
+	}
+
+}
